deeeac832acf23fef760279d4a11bdfc928036f4
Parke <parkermauro11@gmail.com>
2023-02-27 11:41:43 +0800

    exercise

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,9 @@ public class Main{
 
         /* Repeat the above code to create as many objects for different types of member susbcription  
         */
-        
+        ClubAurora clubB = new ClubAurora("CZARX234RT3", "Bob", "Steve", "Winewood Av., New York City, NY", "August 13, 1987", "Half yearly");
+        System.out.println("The member " + clubB.getFirstName() + " " + clubB.getLastName() + " has a " + clubB.getMembership() +
+                    " membership and must pay a monthly subscription fee of: €" + clubB.getSubscriptionFee(clubB.getMembership()));
 
     }
 }
